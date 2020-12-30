8de2977466175b9a3e58b642ee07143dd67cab5d
Richard Neumann <r.neumann@homeinfo.de>
2020-12-30 17:37:33 +0100

    Added os-dependent DATA_FILES.

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 #! /usr/bin/env python
 """Installation script."""
 
+from os import name
 from setuptools import setup
+
+
+if name == 'posix':
+    DATA_FILES = [
+        ('/usr/lib/systemd/system', ['ddnss@.service', 'ddnss@.timer']),
+        ('/usr/lib/sysusers.d', ['ddnss.conf'])
+    ]
+else:
+    DATA_FILES = []
+
 
 setup(
     name='ddnss',
@@ -12,6 +23,7 @@ setup(
     python_requires='>=3.8',
     py_modules=['ddnss'],
     entry_points={'console_scripts': ['ddnssupd = ddnss:main']},
+    data_files=DATA_FILES,
     url='https://github.com/conqp/ddnss',
     license='GPLv3',
     description='Update DynDNS hosts registered at ddnss.de.',
