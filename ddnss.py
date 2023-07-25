579ed04c08e6e1a4202c7ca9f7f6a26fae3883bc
Richard Neumann <r.neumann@homeinfo.de>
2023-07-25 22:22:03 +0200

    Black

--- ddnss.py
+++ ddnss.py
@@ -11,22 +11,22 @@ from urllib.parse import urlencode, urlunparse
 from urllib.request import urlopen
 
 
-__all__ = ['UpdateError', 'update']
+__all__ = ["UpdateError", "update"]
 
 
-if name == 'posix':
-    CONFIG_FILE = Path('/etc/ddnss.conf')
-elif name == 'nt':
-    CONFIG_FILE = Path(getenv('LOCALAPPDATA')) / 'ddnss.conf'
+if name == "posix":
+    CONFIG_FILE = Path("/etc/ddnss.conf")
+elif name == "nt":
+    CONFIG_FILE = Path(getenv("LOCALAPPDATA")) / "ddnss.conf"
 else:
     raise NotImplementedError(f'Operating system "{name}" is not supported.')
 
 
-LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
+LOG_FORMAT = "[%(levelname)s] %(name)s: %(message)s"
 LOGGER = getLogger(Path(__file__).stem)
-REGEX = r'(Updated \d+ hostname\.)'
-URL = ('https', 'ddnss.de', 'upd.php')
-URLv4 = ('https', 'ip4.ddnss.de', 'upd.php')
+REGEX = r"(Updated \d+ hostname\.)"
+URL = ("https", "ddnss.de", "upd.php")
+URLv4 = ("https", "ip4.ddnss.de", "upd.php")
 
 
 class UpdateError(Exception):
@@ -36,7 +36,7 @@ class UpdateError(Exception):
 def update_url(url: str) -> str:
     """Update the respective URL."""
 
-    LOGGER.debug('Updating URL: %s', url)
+    LOGGER.debug("Updating URL: %s", url)
 
     with urlopen(url) as response:
         text = response.read().decode()
@@ -56,25 +56,25 @@ def get_url(params: str, ipv4: bool) -> str:
 def update(host: str, key: str, *, ipv4: bool = False) -> str:
     """Update the respective host using the provided key."""
 
-    return update_url(get_url(urlencode({'host': host, 'key': key}), ipv4))
+    return update_url(get_url(urlencode({"host": host, "key": key}), ipv4))
 
 
 def get_args() -> Namespace:
     """Parse the CLI arguments."""
 
-    parser = ArgumentParser(description='Update ddnss.de domains.')
-    parser.add_argument('host', help='the host to update')
+    parser = ArgumentParser(description="Update ddnss.de domains.")
+    parser.add_argument("host", help="the host to update")
     parser.add_argument(
-        '-f', '--config-file', type=Path, default=CONFIG_FILE,
-        metavar='file', help='the config file to use'
+        "-f",
+        "--config-file",
+        type=Path,
+        default=CONFIG_FILE,
+        metavar="file",
+        help="the config file to use",
     )
-    parser.add_argument('-k', '--key', metavar='key', help='the update key')
-    parser.add_argument(
-        '-4', '--ipv4', action='store_true', help='force IPv4 address'
-    )
-    parser.add_argument(
-        '-v', '--verbose', action='store_true', help='verbose logging'
-    )
+    parser.add_argument("-k", "--key", metavar="key", help="the update key")
+    parser.add_argument("-4", "--ipv4", action="store_true", help="force IPv4 address")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose logging")
     return parser.parse_args()
 
 
@@ -85,11 +85,11 @@ def main() -> int:
     basicConfig(level=DEBUG if args.verbose else INFO, format=LOG_FORMAT)
     config = ConfigParser()
     config.read(args.config_file)
-    ipv4 = config.getboolean(args.host, 'ipv4', fallback=args.ipv4)
+    ipv4 = config.getboolean(args.host, "ipv4", fallback=args.ipv4)
 
     if (key := args.key) is None:
         try:
-            key = config.get(args.host, 'key')
+            key = config.get(args.host, "key")
         except KeyError:
             LOGGER.error('No key configured for host "%s".', args.host)
             return 2
@@ -97,11 +97,11 @@ def main() -> int:
     try:
         message = update(args.host, key, ipv4=ipv4)
     except URLError as error:
-        LOGGER.error('Failed to connect to service.')
+        LOGGER.error("Failed to connect to service.")
         LOGGER.debug(error)
         return 3
     except UpdateError as error:
-        LOGGER.error('Failed to update host.')
+        LOGGER.error("Failed to update host.")
         LOGGER.debug(error)
         return 4
 
